0e36885c7aa3d824f4062cab4ade289faee1b6c5
Sean Meyers <smmeyers@ualberta.ca>
2022-11-04 06:38:16 -0600

    Got the most of the groundwork for the main gameplay loop working. Also, made .sh files, that when run in git bash terminal (type "./import.sh" for example) will automatically compile, decompile, and move the twee and html files. These scripts should work on any OS as long as bash is installed, which it should be since you need it for git. Regarding gameplay stuff, made a basic day, with some basic stats that determine some outcomes. Gonna have to show it to give a better explanation than that. Also, it's best to not push the html file to the repo. I can already smell the merge conflicts... Only push the twee file.

---
+++ export.sh
@@ -0,0 +1,21 @@
+story="TheFix"
+
+# Change this if your Twine story directory is somewhere else.
+# Note: if it can't find the directory no matter what, you may need to run this
+# script from a bash shell directly (just type "./import.sh" in git bash). in my
+# case, it couldn't find the right $HOME because it was using the wrong bash
+# when i typed "bash import.sh" in pwsh. This prolly won't be an issue if you
+# don't have wsl like i do tho.
+twineStoryDir="$HOME/Documents/Twine/Stories"
+
+# Change this to be your tweego command, if tweego is in your path it may be
+# just "tweego" or "tweego.exe", otherwise, you may need to write the full path
+# to the program like I have here.
+tweego="../tweego-2.1.1-windows-x64/tweego.exe"
+
+
+# Compile the twee file from html
+$tweego -d -o "$twineStoryDir/$story.twee"  "$twineStoryDir/$story.html"
+
+# Move to repo directory
+mv -i "$twineStoryDir/$story.twee" $PWD
